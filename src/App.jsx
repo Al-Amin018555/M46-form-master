bf10f196eee4b8edc21ea4518dcb2802db1ee515
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-13 11:30:19 +0600

    learnt reuseable componentns and children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 import './App.css'
-import HookForm from './components/SimpleForm/HookForm/HookForm'
-import RefForm from './components/SimpleForm/RefForm/RefForm'
-import StatefullForm from './components/SimpleForm/StatefullForm/StatefullForm'
+import ReuseableForm from './components/ReuseableForm/ReuseableForm'
+// import HookForm from './components/SimpleForm/HookForm/HookForm'
+// import RefForm from './components/SimpleForm/RefForm/RefForm'
+// import StatefullForm from './components/SimpleForm/StatefullForm/StatefullForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
 
+  const handleSignUpSubmit = data => {
+    console.log(data)
+  }
+  const handleUpdateProfile = data => {
+    console.log(data)
+  }
   return (
     <>
-     {/* <SimpleForm></SimpleForm> */}
+      {/* <SimpleForm></SimpleForm> */}
 
-     {/* way-2 */}
-     {/* <StatefullForm></StatefullForm> */}
-     {/* <RefForm></RefForm> */}
-     <HookForm></HookForm>
+      {/* way-2 */}
+      {/* <StatefullForm></StatefullForm> */}
+      {/* <RefForm></RefForm> */}
+      {/* <HookForm></HookForm> */}
+      <ReuseableForm
+        formTitle='Sign UP'
+        handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sing UP</h2>
+          <p>Please sing up right now</p>
+        </div>
+      </ReuseableForm>
+      <ReuseableForm
+        formTitle='Profile Update'
+        submitBtnText='Update'
+        handleSubmit={handleUpdateProfile}>
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReuseableForm>
+
     </>
   )
 }
--- src/__form__.js
+++ src/__form__.js
@@ -7,4 +7,8 @@
  * way-2: controlled element:- use individual field state for each input field
  * way-3: controlled element:- one objet in a sate holding all the input fields value
  * way-4: uncontrolled element: useRef to create a reference to the element and access value by using like: nameRef.current.value
+ * 
+ * way-5: use hook to handle states and submit and error example: react hook form
+ * way-6: reusable component
+
  */
